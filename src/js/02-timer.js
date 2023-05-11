c409f8eb082577e1f51100cb223876926a4d61a9
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-05-11 21:41:12 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,47 +1,39 @@
-Clockface:document.querySelector('.js-clocface');
-const timer = {
-    intervalId:null,
-    isActive:false,
-    start(){
-        if (this.isActive){
-            return;
-        }
-        const startTime = Data.now();
-        this.isActive=true;
+import flatpickr from  "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
-        this.intervalId=setInterval( ()=>{
-            const currentTime=Date.now();
-            const deltaTime=currentTime - startTime;
-            const { hours, mins, secs}= getTimeComponents(deltaTime);
-            console.log('${hours}:${mins}:${secs}');
-          
-        }, 1000 );
+function flatpickr(selector, options){
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
     },
-    stop(){
-        clearInterval(this.intervalId);
-        this.isActive=false;
-    },
-};
-refs.startBtn.addEventListener('clic', () =>{
-    timer.start();
-});
-refs.stopBtn.addEventListener('clic', () =>{
-    timer.stop();
-});
-function updateClockface({hours,mins,secs}){
-    refs.Clockface.textContent='${hours}:${mins}:${secs}';
+  };
 }
 
-function pad(value){
-    return String(value).padStart(2,'0');
-}
 
-function getTimeComponents(time){
-    
-const hours= pad(
-    Math.floor((time%(1000*60*60*24))/(1000*60*60)),
-);
-const mins=pad(Math.floor((time%(1000*60*60))/(1000*60)));
-const secs= pad(Math.floor((time%(1000*60))/1000));
-return {hours,mins,secs};
-}
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
