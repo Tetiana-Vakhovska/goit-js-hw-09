31a96b964df99dfb0940fcbc06e8ab01e6738db2
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-05-15 08:30:27 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,17 +2,6 @@ import flatpickr from  "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-function flatpickr(selector, options){
-  const options = {
-   
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-}
 
 
   function convertMs(ms) {
