0836131d9178b668f3bf010bb0b225068876e448
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-06-28 22:25:48 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,7 +10,7 @@ const refs = {
   mins: document.querySelector('span[data-minutes]'),
   secs: document.querySelector('span[data-seconds]'),
 };
-let differenceInTime;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -30,7 +30,7 @@ const options = {
 
     refs.start.addEventListener('click', () => {
       intervalId = setInterval(() => {
-        const differenceInTime = selectedDates[0] - new Date();
+        const differenceInTime = selectedDates[0] - Date.now();
 
         if (differenceInTime < 1000) {
           clearInterval(intervalId);
